22757306f76f9f9f9c4843a256c2aa630dfeb8c1
Дмитрий Сологубов <Sologubov.D.V@gmail.com>
2019-09-06 11:34:45 +0300

    Добавьте файлы проекта.

---
+++ mySchema/ExtСommands/ConnectionDB.cs
@@ -0,0 +1,62 @@
+﻿
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+using System;
+using System.Data.OleDb;
+using System.Windows;
+
+namespace ConnectionDB
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    { 
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            //string dbFilePath = "D:\\_DB_project.mdb";
+            string dbFilePath = "D:\\_DB_project_poligon.accdb";
+
+
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dbFilePath;
+
+
+            OleDbConnection dbConnection = new OleDbConnection();
+            dbConnection.ConnectionString = connectionString;
+
+
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = dbConnection;
+
+            try
+            {
+                dbConnection.Open();
+
+                MessageBox.Show("Подключились к БД");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка получения данных в БД: " +
+                                 Environment.NewLine +
+                                 ex.Message,
+                                 "Ошибка");
+            }
+            finally
+            {
+                dbConnection.Close();
+                dbConnection.Dispose();
+            }
+
+
+            return Result.Succeeded;
+        }
+
+
+
+    }
+
+
+
+
+}
---
+++ mySchema/ExtСommands/_POLIGON.cs
@@ -0,0 +1,77 @@
+﻿
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Data.OleDb;
+using System.Windows;
+
+namespace POLIGON
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ACommand : IExternalCommand
+    {
+        public static string RP_name1;
+        public static UIApplication uiApp;
+        public static UIDocument uiDoc;
+        public static Autodesk.Revit.ApplicationServices.Application app;
+        public static Document doc;
+        public static Selection sel;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            uiApp = commandData.Application;
+            uiDoc = uiApp.ActiveUIDocument;
+            app = uiApp.Application;
+            doc = uiDoc.Document;
+            sel = uiDoc.Selection;
+
+
+
+            //string dbFilePath = "D:\\_DB_project.mdb";
+            string dbFilePath = "D:\\_DB_project_poligon.accdb";
+
+
+            string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dbFilePath;
+
+
+            OleDbConnection dbConnection = new OleDbConnection();
+            dbConnection.ConnectionString = connectionString;
+
+
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = dbConnection;
+
+            try
+            {
+                dbConnection.Open();
+
+                MessageBox.Show("Подключились к БД");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка получения данных в БД: " +
+                                 Environment.NewLine +
+                                 ex.Message,
+                                 "Ошибка");
+            }
+            finally
+            {
+                dbConnection.Close();
+                dbConnection.Dispose();
+            }
+
+
+            return Result.Succeeded;
+        }
+
+
+
+    }
+
+
+
+
+}
